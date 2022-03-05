e9185be495067d21c3bc413178c0d8c11a5d9d61
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-05 16:16:14 -0500

    linked files to one another

---
+++ index.js
@@ -0,0 +1,11 @@
+// Requirments needed for application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const jest = require('jest');
+//const path = require('path');
+
+// Constructors
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
