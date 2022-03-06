11a384242f7ad401c33bf1f0ced07f62395466b1
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-06 10:44:34 -0500

    adding intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,18 @@
 const Intern = require('../lib/Intern');
+const Employee = require('../lib/Employee');
+
+test('set school', () => {
+    const value = 'GCA';
+    const intern = new Intern ('Tari', 1, 'tari@gmail.com', value);
+    expect(intern.school).toBe(value);
+});
+test('get role', () => {
+    const value = 'Intern';
+    const intern = new Intern ('Tari', 1, 'tari@gmail.com', 'GCA');
+    expect(intern.getRole()).toBe(value);
+});
+test('intern via getSchool', () => {
+    const value = 'GCA';
+    const intern = new Intern ('Tari', 1, 'tari@gmail.com', value);
+    expect(intern.getSchool()).toBe(value);
+});
