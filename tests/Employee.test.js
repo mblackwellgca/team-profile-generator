10b058a68ac77d4ce752483d4ab2c59d390c979c
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-06 09:40:28 -0500

    adding Employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,20 @@
 const Employee = require('../lib/Employee');
+
+test('create employee', () => {
+    const employee = new Employee('Mikki');
+});
+test('set id', () => {
+    const value = 100000;
+    const employee = new Employee('Mikki', value);
+    expect(employee.id).toBe(value);
+});
+test('set email', () => {
+    const value = 'employee@email.com';
+    const employee = new Employee('Mikki', 1, value);
+    expect(employee.email).toBe(value);
+});
+test('set role', () => {
+    const value = 'Employee';
+    const employee = new Employee('Mikki', 1, 'employee@email.com');
+    expect(employee.getRole()).toBe(value);
+});
