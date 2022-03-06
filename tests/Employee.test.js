8e40f95f11dad2b6749165deffdfd6dcb78dbfbf
Michelle Blackwell <92054570+mblackwellgca@users.noreply.github.com>
2022-03-06 10:32:29 -0500

    adding manager test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,20 +1,23 @@
 const Employee = require('../lib/Employee');
 
-test('create employee', () => {
-    const employee = new Employee('Mikki');
-});
-test('set id', () => {
-    const value = 100000;
-    const employee = new Employee('Mikki', value);
-    expect(employee.id).toBe(value);
-});
-test('set email', () => {
-    const value = 'employee@email.com';
-    const employee = new Employee('Mikki', 1, value);
-    expect(employee.email).toBe(value);
-});
-test('set role', () => {
-    const value = 'Employee';
-    const employee = new Employee('Mikki', 1, 'employee@email.com');
-    expect(employee.getRole()).toBe(value);
-});
+describe('Employee' , () => { 
+
+    test('create employee', () => {
+        const employee = new Employee('Mikki');
+    });
+    test('set id', () => {
+        const value = 100000;
+        const employee = new Employee('Mikki', value);
+        expect(employee.id).toBe(value);
+    });
+    test('set email', () => {
+        const value = 'employee@email.com';
+        const employee = new Employee('Mikki', 1, value);
+        expect(employee.email).toBe(value);
+    });
+    test('set role', () => {
+        const value = 'Employee';
+        const employee = new Employee('Mikki', 1, 'employee@email.com');
+        expect(employee.getRole()).toBe(value);
+    });
+})
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,18 @@
 const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test('manager office number', () => {
+    const value = 100;
+    const manager = new Manager ('Tari', 1, 'tari@gmail.com', value);
+    expect(manager.officeNumber).toBe(value);
+});
+test('get role', () => {
+    const value = 'Manager';
+    const manager = new Manager ('Tari', 1, 'tari@gmail.com', 100);
+    expect(manager.getRole()).toBe(value);
+});
+test('manager office number via getOfficeNumber', () => {
+    const value = 100;
+    const manager = new Manager ('Tari', 1, 'tari@gmail.com', value);
+    expect(manager.officeNumber()).toBe(value);
+});
